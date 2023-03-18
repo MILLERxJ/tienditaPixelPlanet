881a281311c9f25c9b36e75ac129c5a12ce0d2ba
MILLERxJ <contactomillerxj@gmail.com>
2023-03-17 20:42:33 -0500

    Se crea el index, container y dos section

---
+++ src/AcercaDe/AcercaDe.js
@@ -0,0 +1,19 @@
+export function AcercaDe() {
+    return (
+        <>
+            <section>
+                <div class="container my-5">
+                    <div class="row justify-content-center">
+                        <div class="col-12 col-md-4">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/tiendadejuancho-a1ef3.appspot.com/o/elden%20ring.webp?alt=media&token=1c792c6d-9a9c-4442-ba86-5ce50fb37057" alt="foto" class="img-fluid" />
+                        </div>
+                        <div class="col-12 col-md-4 text-center align-self-center">
+                            <h2>Elden Ring</h2>
+                            <p>Un juego de From Software de RPG y de mundo abierto</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+} 
